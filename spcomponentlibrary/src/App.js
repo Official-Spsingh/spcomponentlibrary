21d4bb803f7885965c015ab6b3f55fee2d131a19
Shubham Pratap Singh <SPSingh@bpl.netlink.com>
2020-11-18 22:16:52 +0530

    changes git commit -m changes

---
+++ spcomponentlibrary/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Test1 from 'sp-react-package1';
+
+function App() {
+  return (
+    <div>
+      <Test1 title="Helloa" paragraph="jaaj" bgColor="blue" />
+    </div>
+  );
+}
+
+export default App;
